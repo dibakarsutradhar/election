6c69185df8f5b1ed825d5842267eabc201c70cfb
Dibakar <crossbonesdipu@gmail.com>
2018-12-03 20:04:56 +0600

    cast vote test passed

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,10 +8,11 @@ contract Election {
         uint voteCount;
     }
 
+    // Sote accounts that have voted
+    mapping(address => bool) public voters;
     // Store Candiadates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
-
     // Store Candidates Count
     uint public candidatesCount;
 
@@ -24,5 +25,19 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount ++;
     }
 }
--- test/election.js
+++ test/election.js
@@ -26,4 +26,61 @@ contract("Election", function (accounts) {
             assert.equal(candidate[2], 0, "contains the correct votes count");
         });
     });
+
+    it("allows a voter to cast a vote", function() {
+        return Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            candidateId = 1;
+            return electionInstance.vote(candidateId, { from: accounts[0] });
+        }).then(function(receipt) {
+            return electionInstance.voters(accounts[0]);
+        }).then(function(voted) {
+            assert(voted, "the voter was marked as voted");
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate) {
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "increments the candidate's vote count");
+        });
+    });
+
+    it("throws an exception for invalid candidates", function () {
+        return Election.deployed().then(function (instance) {
+            electionInstance = instance;
+            return electionInstance.vote(99, { from: accounts[1] })
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+            return electionInstance.candidates(1);
+        }).then(function (candidate1) {
+            var voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+            return electionInstance.candidates(2);
+        }).then(function (candidate2) {
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
+        });
+    });
+
+    it("throws an exception for double voting", function () {
+        return Election.deployed().then(function (instance) {
+            electionInstance = instance;
+            candidateId = 2;
+            electionInstance.vote(candidateId, { from: accounts[1] });
+            return electionInstance.candidates(candidateId);
+        }).then(function (candidate) {
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "accepts first vote");
+            // Try to vote again
+            return electionInstance.vote(candidateId, { from: accounts[1] });
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+            return electionInstance.candidates(1);
+        }).then(function (candidate1) {
+            var voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+            return electionInstance.candidates(2);
+        }).then(function (candidate2) {
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 1, "candidate 2 did not receive any votes");
+        });
+    });
 });
