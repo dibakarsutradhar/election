705fcfe64ca512b16769191cbce3f6e7fb282d79
Dibakar <crossbonesdipu@gmail.com>
2018-12-03 20:14:16 +0600

    cast vote

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,11 @@ App = {
   account: '0x0',
   hasVoted: false,
 
-  init: function() {
+  init: function () {
     return App.initWeb3();
   },
 
-  initWeb3: function() {
+  initWeb3: function () {
     // TODO: refactor conditional
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
@@ -22,39 +22,37 @@ App = {
     return App.initContract();
   },
 
-  initContract: function() {
-    $.getJSON("Election.json", function(election) {
+  initContract: function () {
+    $.getJSON("Election.json", function (election) {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
 
-      //App.listenForEvents();
+      App.listenForEvents();
 
       return App.render();
     });
   },
 
-  /*
   // Listen for events emitted from the contract
-  listenForEvents: function() {
-    App.contracts.Election.deployed().then(function(instance) {
+  listenForEvents: function () {
+    App.contracts.Election.deployed().then(function (instance) {
       // Restart Chrome if you are unable to receive this event
       // This is a known issue with Metamask
       // https://github.com/MetaMask/metamask-extension/issues/2393
       instance.votedEvent({}, {
         fromBlock: 0,
         toBlock: 'latest'
-      }).watch(function(error, event) {
+      }).watch(function (error, event) {
         console.log("event triggered", event)
         // Reload when a new vote is recorded
         App.render();
       });
     });
   },
-  */
 
-  render: function() {
+  render: function () {
     var electionInstance;
     var loader = $("#loader");
     var content = $("#content");
@@ -63,7 +61,7 @@ App = {
     content.hide();
 
     // Load account data
-    web3.eth.getCoinbase(function(err, account) {
+    web3.eth.getCoinbase(function (err, account) {
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
@@ -71,10 +69,10 @@ App = {
     });
 
     // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
+    App.contracts.Election.deployed().then(function (instance) {
       electionInstance = instance;
       return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
+    }).then(function (candidatesCount) {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
@@ -82,7 +80,7 @@ App = {
       candidatesSelect.empty();
 
       for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
+        electionInstance.candidates(i).then(function (candidate) {
           var id = candidate[0];
           var name = candidate[1];
           var voteCount = candidate[2];
@@ -90,41 +88,41 @@ App = {
           // Render candidate Result
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
-          /*
+
           // Render candidate ballot option
           var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-          candidatesSelect.append(candidateOption); */
+          candidatesSelect.append(candidateOption);
         });
-      } /*
+      }
       return electionInstance.voters(App.account);
-    }).then(function(hasVoted) {
+    }).then(function (hasVoted) {
       // Do not allow a user to vote
-      if(hasVoted) {
+      if (hasVoted) {
         $('form').hide();
-      } */
+      }
       loader.hide();
       content.show();
-    }).catch(function(error) {
+    }).catch(function (error) {
       console.warn(error);
     });
-  }
-/*
-  castVote: function() {
+  },
+
+  castVote: function () {
     var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed().then(function(instance) {
+    App.contracts.Election.deployed().then(function (instance) {
       return instance.vote(candidateId, { from: App.account });
-    }).then(function(result) {
+    }).then(function (result) {
       // Wait for votes to update
       $("#content").hide();
       $("#loader").show();
-    }).catch(function(err) {
+    }).catch(function (err) {
       console.error(err);
     });
-  } */
+  }
 };
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
